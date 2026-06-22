9e6f2df54e7f1a3b1cf3269ada049831dc3a77db
Hariom Sharma <harifmusician@gmail.com>
2026-06-22 22:22:32 +0530

    Added confession timer

--- missme.js
+++ missme.js
@@ -1,7 +1,6 @@
 async function sendAlert(message){
 
-    const webhook =
-    "https://discord.com/api/webhooks/1518645891827761265/qC2vc2uWAWGEadbr1hIZHROzGROil0EEk9WmcwYEduR2etGJWjJERloVnsf2okhm2KVj";
+    const webhook = "https://discord.com/api/webhooks/1518645891827761265/qC2vc2uWAWGEadbr1hIZHROzGROil0EEk9WmcwYEduR2etGJWjJERloVnsf2okhm2KVj";
 
     try{
 
@@ -16,13 +15,18 @@ async function sendAlert(message){
 
             body: JSON.stringify({
 
-                content: message
+                content:
+                `📡 PRI SIGNAL RECEIVED
+
+${message}
+
+🕒 ${new Date().toLocaleTimeString()}`
 
             })
 
         });
 
-        alert("Sent ❤️");
+        alert("Signal sent ❤️");
 
     }
 
